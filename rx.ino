793371aa07e1b0c468380c640a346cd30d52f2c6
Satwik Agarwal <asatwik218@gmail.com>
2022-11-20 15:50:33 +0530

    added getLoadCellVal()

--- rx.ino
+++ rx.ino
@@ -13,16 +13,19 @@ HX711_ADC LoadCell(HX711_dout, HX711_sck);
 SoftwareSerial Xbee(4,5); // RX, TX
 
 
+unsigned long t = 0;
 int fire_state = 0;
 
 //TODO: get pressure transducer and the load cell readings
 void setup(){
+    //fire mosfet
     pinMode(fire_mosfet , OUTPUT);
 
     LoadCell.begin();
 
     // ! calibration value to be changed
     float calibrationVal = 696.0;
+    
     unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
     boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
     LoadCell.start(stabilizingtime, _tare);
@@ -54,12 +57,46 @@ void loop(){
 
     if(fire_state == 1){
         //TODO: send data back 
+        getLoadCellVal();
+        getPressTransVal();
     }
 
 
 
 }
 
-void getLoadCellVal(){
+float getPressTransVal(){
 
+}
+
+float getLoadCellVal(){
+    static boolean newDataReady = 0;
+    const int serialPrintInterval = 0; //increase value to slow down serial print activity
+    float i;
+    // check for new data/start next conversion:
+    if (LoadCell.update()) newDataReady = true;
+
+    // get smoothed value from the dataset:
+    if (newDataReady) {
+        if (millis() > t + serialPrintInterval) {
+            i = LoadCell.getData();
+            Serial.print("Load_cell output val: ");
+            Serial.println(i);
+            newDataReady = 0;
+            t = millis();
+        }
+    }
+
+    // receive command from serial terminal, send 't' to initiate tare operation:
+    if (Serial.available() > 0) {
+        char inByte = Serial.read();
+        if (inByte == 't') LoadCell.tareNoDelay();
+    }
+
+    // check if last tare operation is complete:
+    if (LoadCell.getTareStatus() == true) {
+        Serial.println("Tare complete");
+    }
+
+    return i;
 }
